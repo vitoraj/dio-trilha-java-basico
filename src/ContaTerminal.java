6162926ac48c3ed5f5a1729dac3249a6cdc46c33
vitoraj <39956858+vitoraj@users.noreply.github.com>
2023-09-09 12:41:55 -0300

    Update ContaTerminal.java
    Simulado final

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main (String [] args) {
+
+        Scanner sc = new Scanner (System.in).useLocale(Locale.US);
+
+        
+
+     
+        System.out.println("Digite o seu nome completo:");
+        String nome = sc.nextLine();
+
+        System.out.println("Digite a sua Agencia:");
+        String agencia = sc.next();
+
+        System.out.println("Digite o numenro da sua conta:");
+        int conta = sc.nextInt();
+
+        System.out.println("Informe seu saldo inicial:");
+        double saldo = sc.nextDouble();   
+
+       System.out.println("Ola, " +nome+ " obrigado por criar uma conta em nosso banco, sua agência é " +agencia+ " sua conta " +conta+ " seu saldo é R$ " +saldo+ " e já está disponível para saque !");
+
     }
+    
 }
+
